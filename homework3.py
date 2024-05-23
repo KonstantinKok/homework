4eff8197d38e2025a15f3a8b7e8f1071bffec9f6
KonstantinKok <konstantin.nikolaevish.14.11@gmail.com>
2024-05-23 18:50:59 +0300

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -1,8 +1,8 @@
 name ='Konstantin'
-print(name)
+print('Name:', name)
 age = 32
-print(age)
+print('Age:', age)
 age = (age + 1)
-print(age)
+print('New age:',  age)
 is_student = True
-print(is_student)
+print('Is student:',  is_student)
