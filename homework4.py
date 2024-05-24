29185f15500ac259366f59bc2f539c304bdb9043
KonstantinKok <konstantin.nikolaevish.14.11@gmail.com>
2024-05-24 17:08:04 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,3 @@
+my_string = input('Напишите текст используя не менее 1 пробела, но не обязательно: ')
+print(f"{len(my_string)}\n{my_string .upper()}\n{my_string .lower()}\n"
+      f"{my_string .replace(' ','')}\n{my_string[0]}\n{my_string[-1]}")
