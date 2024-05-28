461c84a99572f9909dd7d9386c30317d225aedee
KonstantinKok <konstantin.nikolaevish.14.11@gmail.com>
2024-05-28 15:32:07 +0300

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -1,9 +1,11 @@
 first, second, third = (int(input('Введите первое число: ')),
                         int(input('Введите второе число: ')),
                         int(input('Введите третье число: ' )))
-if first == second == first == third:
+
+
+if first == second == third:
     print('3')
 elif first == second or first == third or second == third:
     print('2')
-elif first != second != third:
+else:
     print('0')
