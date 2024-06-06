bc8525b74808338164c228cdbb11a9ec40fdb1ca
KonstantinKok <konstantin.nikolaevish.14.11@gmail.com>
2024-06-06 19:43:28 +0300

    Add files via upload

---
+++ module_2_7.py
@@ -0,0 +1,23 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+
+# print_params()
+# print_params(2)
+# print_params(3, 'замена строки')
+# print_params(4, 'все замены', False)
+# print_params(c=False)
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
+
+values_list = [1, 'Hello', True]
+values_dict = {'a': 1, 'b': 'Hello', 'c': True}
+print_params(*values_list)
+print_params(**values_dict)
+
+
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
+
+
