49ff7a4d01e79eb10fc74db0aba15cff0d84a907
KonstantinKok <konstantin.nikolaevish.14.11@gmail.com>
2024-06-25 13:27:27 +0300

    Update module_5_1.py

--- module_5_1.py
+++ module_5_1.py
@@ -4,7 +4,7 @@ class House:
         self.number_of_floors = number_of_floors
 
     def go_to(self, new_floor):
-        if new_floor == 0 or new_floor > self.number_of_floors:
+        if new_floor <= 0 or new_floor > self.number_of_floors:
             print('Такого этажа не существует')
             return
         for new_floor in range(1, new_floor + 1):
