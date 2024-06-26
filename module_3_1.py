192787f7f813cf52b14cd5e6cc2294f5c982bf67
KonstantinKok <konstantin.nikolaevish.14.11@gmail.com>
2024-06-26 20:28:21 +0300

    Add files via upload

--- module_3_1.py
+++ module_3_1.py
@@ -1,43 +1,29 @@
 calls = 0
 
 
-# подсчитывающая вызовы остальных функций
-
-
 def count_calls():
     global calls
-
-
-# принимает аргумент - строку и возвращает кортеж из: длины этой строки, строку в верхнем регистре,
-# строку в нижнем регистре.
+    calls += 1
 
 
 def string_info(string):
-    string.upper()
-    string.lower()
-
-
-# принимает два аргумента: строку и список, и возвращает True,
-# если строка находится в этом списке, False - если отсутствует.
-# Регистром строки при проверке пренебречь: UrbaN ~ URBAN.
+    count_calls()
+    length = len(string)
+    return length, string.upper(), string.lower()
 
 
 def is_contains(string, list_to_search):
-    pass
+    count_calls()
+    new_string = string.lower()
+    new_list = [x.lower() for x in list_to_search]
+    if new_string in new_list:
+        return True
+    else:
+        return False
 
 
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBan
-print(is_contains('cycle', ['recycle', 'cyclic']))  # No matches
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycle', 'cyclic']))
 print(calls)
-
-# Вывод на консоль:
-# (8, 'CAPYBARA', 'capybara')
-# (10, 'ARMAGEDDON', 'armageddon')
-# True
-# False
-# 4
-# Примечания:
-# Для использования глобальной переменной внутри функции используйте оператор global.
-# Для функции is_contains лучше привести и искомую строку и все строки в списке в один регистр.
