7a4e112612d768eacdeb97da91ad425fecf4b4c2
KonstantinKok <konstantin.nikolaevish.14.11@gmail.com>
2024-07-05 12:29:24 +0300

    Add files via upload

---
+++ module_5_3(1).py
@@ -0,0 +1,33 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        args = args[0]
+        cls.houses_history.append(args)
+        return object.__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+        return
+
+    def __add__(self, other):
+        self.name += other
+        return self
+
+    def __del__(self):
+        return print(f"{self.name} снесён, но он останется в истории.")
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
