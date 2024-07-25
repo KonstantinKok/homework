a79709213395a56745a3cb8ae8f5b2a691f65aae
KonstantinKok <konstantin.nikolaevish.14.11@gmail.com>
2024-07-25 09:41:53 +0300

    Add files via upload

---
+++ module_7_3.py
@@ -0,0 +1,46 @@
+import io
+from pprint import pprint
+# Ссылка на проверочный файл https://github.com/KonstantinKok/homework/blob/main/test_file.txt
+
+
+class WordsFinder:
+
+    def __init__(self, *file_names):
+        self.file_names = file_names
+
+    def get_all_words(self):
+        all_words = {}
+        for file_name in self.file_names:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                words = []
+                for i in file:
+                    i = i.lower()
+                    punctuation = [',', '.', '=', '!', '?', ';', ':']
+                    for j in punctuation:
+                        i = i.replace(j, '')
+                    i = i.replace(' - ', ' ')
+                    words.extend(i.split())
+                all_words[file_name] = words
+        return all_words
+
+    def find(self, word):
+        places = {}
+        for key, value in self.get_all_words().items():
+            if word.lower() in value:
+                places[key] = value.index(word.lower()) + 1
+
+        return places
+
+    def count(self, word):
+        counters = {}
+        for value, key in self.get_all_words().items():
+            words_count = key.count(word.lower())
+            counters[value] = words_count
+
+        return counters
+
+
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words())    # Все слова
+print(finder2.find('TEXT'))    # 3 слово по счёту
+print(finder2.count('teXT'))    # 4 слова teXT в тексте всего
