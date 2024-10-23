146a9732e5349ecfe2ba37400ef260bcc280ea6f
KonstantinKok <konstantin.nikolaevish.14.11@gmail.com>
2024-10-23 14:28:54 +0300

    Update module_9_4.py

--- module_9_4.py
+++ module_9_4.py
@@ -1,32 +1,34 @@
-import random
-from random import choice
+from  random import choice
 
 first = 'Мама мыла раму'
 second = 'Рамена мало было'
 
+result = list(map(lambda x, y: x == y, first, second))
+print(result)
 
 def get_advanced_writer(file_name):
-    def write_everything(*data_set):
-        with open(file_name, 'a', encoding='utf8') as file:
-            for data in data_set:
-                file.write(str(data) + '\n')
 
-    return write_everything
+        def write_everything(*data_set):
+            with open(file_name, mode='w', encoding='UTF-8') as file:
+                for i in data_set:
+                    file.write(str(i))
+                    file.write('\n')
 
+        return write_everything
 
 write = get_advanced_writer('example.txt')
 write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
 
-
 class MysticBall:
-    def __init__(self, *words):
-        self.words = words
+    def __init__(self, *word):
+        self.words = word
 
     def __call__(self):
-        return random.choice(self.words)
+        word = choice(self.words)
+        return word
 
 
-first_ball = MysticBall('Да', 'Нет', 'Наверное', 'Странно')
+first_ball = MysticBall('Да', 'Нет', 'Наверное', 'Может быть', 'Никогда')
 print(first_ball())
 print(first_ball())
 print(first_ball())
