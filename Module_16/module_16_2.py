25f0025e4d9b72db166946ab21dbe411de5cdd77
KonstantinKok <konstantin.nikolaevish.14.11@gmail.com>
2024-11-05 15:40:36 +0300

    Add files via upload

--- Module_16/module_16_2.py
+++ Module_16/module_16_2.py
@@ -16,8 +16,7 @@ async def id_paginator(user_id: int = Path(ge=1, le=100, description="Enter User
     return {"message": f"Hello {user_id}"}
 
 @app.get("/user/{username}/{age}")
-async def news(username: Annotated[ str, Path(min_length=5, max_length=20, description="Enter you username", example="montes")]
-                                   , age: int, ge=18, le=120) -> dict:
+async def news(age: int = Path(min_length=18, max_length=120), username: str = Path(min_length=5, max_length=20, description="Enter you username", example="montes")) -> dict:
     return {"message": f"Hello {username} {age}"}
 
 
